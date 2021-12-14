619d5aede4a4e21b8049ea50b4dea225c99c2a6e
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-14 23:55:12 +0100

    Fix: CI/CD .env file

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,6 +8,6 @@ echo "API_KEY_SECRET=\"${API_KEY_SECRET}\"" > .env
 echo "USER_ACCESS_TOKEN=\"${USER_ACCESS_TOKEN}\"" > .env
 echo "USER_ACCESS_TOKEN_SECRET=\"${USER_ACCESS_TOKEN_SECRET}\"" > .env
 echo "BEARER_TOKEN=\"${BEARER_TOKEN_P1}%${BEARER_TOKEN_P2}%${BEARER_TOKEN_P3}\"" > .env
-echo "REDIS_HOST=\"tweetmetric-redis\"" > .env
+echo "REDIS_HOST=\"tweetmetric-redis\"" >> .env
 
 docker-compose up -d --force-recreate
