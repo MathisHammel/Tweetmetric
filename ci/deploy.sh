fd920ab0dfb218bab3703fa203c794f959ea792c
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-14 23:57:48 +0100

    Fix: CI/CD .env file

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,10 +4,10 @@ REPO_PATH="${PROJECT_HOME}/Tweetmetric/"
 
 cd "${REPO_PATH}" && git pull origin "${GIT_BRANCH}" || :
 echo "API_KEY=\"${API_KEY}\"" > .env
-echo "API_KEY_SECRET=\"${API_KEY_SECRET}\"" > .env
-echo "USER_ACCESS_TOKEN=\"${USER_ACCESS_TOKEN}\"" > .env
-echo "USER_ACCESS_TOKEN_SECRET=\"${USER_ACCESS_TOKEN_SECRET}\"" > .env
-echo "BEARER_TOKEN=\"${BEARER_TOKEN_P1}%${BEARER_TOKEN_P2}%${BEARER_TOKEN_P3}\"" > .env
+echo "API_KEY_SECRET=\"${API_KEY_SECRET}\"" >> .env
+echo "USER_ACCESS_TOKEN=\"${USER_ACCESS_TOKEN}\"" >> .env
+echo "USER_ACCESS_TOKEN_SECRET=\"${USER_ACCESS_TOKEN_SECRET}\"" >> .env
+echo "BEARER_TOKEN=\"${BEARER_TOKEN_P1}%${BEARER_TOKEN_P2}%${BEARER_TOKEN_P3}\"" >> .env
 echo "REDIS_HOST=\"tweetmetric-redis\"" >> .env
 
 docker-compose up -d --force-recreate
