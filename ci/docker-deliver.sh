94683a1392eee33ad3cfc66a0d85d9343f44b8be
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-14 21:21:22 +0100

    Fix: CI/CD

--- ci/docker-deliver.sh
+++ ci/docker-deliver.sh
@@ -10,16 +10,13 @@ tag_and_push() {
   docker push "comworkio/${2}:${1}"
 }
 
-cd "${REPO_PATH}" && git pull origin main || : 
+cd "${REPO_PATH}" && git pull origin "${GIT_BRANCH}" || : 
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_EMAIL}"
 sha="$(git rev-parse --short HEAD)"
 echo '{"version":"'"${VERSION}"'", "sha":"'"${sha}"'", "arch":"'"${ARCH}"'"}' > manifest.json
 
-docker_compose_file="docker-compose.yml"
-[[ -f docker-compose-build-${ARCH}.yml ]] && docker_compose_file="docker-compose.yml"
-
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f "${docker_compose_file}" build "${IMAGE}"
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose build "${IMAGE}"
 
 echo "${DOCKER_ACCESS_TOKEN}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
 
