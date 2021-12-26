cefef88c4e8f7f2a4d71768b174e1967a7fa26c7
Idriss Neumann <neumann.idriss@gmail.com>
2021-12-26 11:45:10 +0000

    Use mirror runner

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-REPO_PATH="${PROJECT_HOME}/Tweetmetric/"
+if [[ ${MIRROR_PROJECT_HOME} ]]; then
+  REPO_PATH="${MIRROR_PROJECT_HOME}/Tweetmetric/"
+else
+  REPO_PATH="${PROJECT_HOME}/Tweetmetric/"
+fi
 
 cd "${REPO_PATH}" && git pull origin "${GIT_BRANCH}" || :
 git push github main
