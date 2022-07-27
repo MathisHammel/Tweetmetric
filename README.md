a8aaad5f6d3ebfeedd8d5c69317dc16cc0287d07
Mathis Hammel <mathis.hammel@gmail.com>
2022-07-27 13:44:56 +0200

    Tweetmetric 2.0 update

--- README.md
+++ README.md
@@ -4,14 +4,7 @@ Tweetmetric allows you to track various metrics on your most recent tweets, such
 
 ![example image](./img/dashboard.png)
 
-The code is in Python, and the frontend uses Dash (a Plotly web interface). Tweetmetric uses Redis as a fast database
-
-## Git repositories
-
-* Build repo: https://gitlab.comwork.io/oss/Tweetmetric (with CI/CD pipelines delivering docker images on docker hub)
-* Github mirror/fork: https://github.com/idrissneumann/Tweetmetric (automatically up to date with the CI/CD)
-* Public gitlab mirror: https://gitlab.com/ineumann/Tweetmetric (automatically up to date with the CI/CD)
-* Original repo: https://github.com/MathisHammel/Tweetmetric
+The code is in Python, and the frontend uses Dash (a Plotly web interface). Tweetmetric uses Redis as a fast database.
 
 ## Docker images
 
@@ -20,21 +13,31 @@ You'll find ready to use images on docker hub:
 * [tweetmetric-fetch-loop](https://hub.docker.com/repository/docker/comworkio/tweetmetric-fetch-loop)
 * [tweetmetric-viz-server](https://hub.docker.com/repository/docker/comworkio/tweetmetric-viz-server)
 
-You'll find how to use those images with this [docker-compose file](./docker-compose.yml).
+Docker images are built and updated from a [mirror repo](https://gitlab.comwork.io/oss/Tweetmetric).
 
 ## Demo
 
-You can access to a [demo here](https://tweetmetric.comwork.io).
-## Getting started
+You can access a [demo here](http://mathis.h25.io:8050) based on @MathisHammel's tweets.
 
-Install `docker` and `docker-compose`.
-
-If you're on windows or mac, you can use [Docker Desktop](https://www.docker.com/products/docker-desktop), you'll have to replace all the `docker-compose` command by `docker compose`.
+## Connect to Twitter
 
 Tweetmetric uses private metrics that can only be accessed by the Tweet's owner. You need to provide your API keys to the program so it can work.
 - Request a Twitter API key on [The Twitter developer portal](https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api). This only takes a couple minutes, you need to have a verified phone number on your account.
 - Generate a user token for the app you just created on [the developer dashboard](https://developer.twitter.com/en/portal/dashboard)
-- You should now have 4 secrets provided by Twitter. Store them in their corresponding strings inside a `.env` file (you can create it from the [`.env.dist`](./.env.dist) example)
+
+## Getting started
+
+### Standalone setup
+
+- Store the Twitter secrets in their corresponding environment variables (variable names are in the `src/api_secrets.py` file)
+- Run `cd src; ./launch.sh`
+
+### Using Docker
+
+Install `docker` and `docker-compose`.
+
+If you're on windows or mac, you can use [Docker Desktop](https://www.docker.com/products/docker-desktop) and use `docker-compose` instead of `docker compose`.
+
 
 ```shell
 $ cp .env.dist .env
@@ -42,13 +45,13 @@ $ cp .env.dist .env
 $ docker-compose up -d
 ```
 
-And that's it.
+Store your Twitter secrets in their corresponding strings inside a `.env` file (you can create it from the [`.env.dist`](./.env.dist) example)
 
-Note: you can pick only the [docker-compose file](./docker-compose.yml) and create your `.env` file without having to clone all this git repository.
+Note: you can pick only the [docker-compose file](./docker-compose.yml) and create your `.env` file without having to clone this git repository.
 
 ## Contributions
 
-If you have to add a python dependancies in order to patch or add some features, please complete the [requirements.txt](./src/requirements.txt).
+If you have to add a python dependency in order to patch or add some features, please update [requirements.txt](./src/requirements.txt) accordingly.
 
 If you need to rebuild the images because you made some changes:
 
--- src/viz_server.py
+++ src/viz_server.py
@@ -1,24 +1,30 @@
-import os
+import collections
 import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 from datetime import datetime
 import json
+import plotly.express as px
 import plotly.graph_objects as go
 import redis
+import pandas as pd
 import pytz
+import time
 
 DISPLAY_CHAR_LIMIT = 100
 FIELDS_ORDER = ['impression_count', 'retweet_count', 'reply_count', 'like_count', 'quote_count', 'user_profile_clicks', 'url_link_clicks']
 TIMEZONE = pytz.timezone('Europe/Paris')
 
-redis_cli = redis.Redis(host=os.environ['REDIS_HOST'], port=int(os.environ['REDIS_PORT']), db=0)
+ALIGN_TIME = 6 * 3600  # 6 hours. Time at which all curves meet
+CUTOFF_TIME = 2 * 86400 # 2 days. Default time span for the displayed graph
+
+redis_cli = redis.Redis(host='localhost', port=6379, db=0)
 
 app = dash.Dash(__name__)
-app.title = os.environ['APP_TITLE']
 
 def render_layout():
+    tweet_contents = {}
     tweet_text = redis_cli.hgetall('tweet_text')
     tweet_creationdate = redis_cli.hgetall('creation_date')
     sorted_tweets_most_recent = []
@@ -27,20 +33,27 @@ def render_layout():
         sorted_tweets_most_recent.append((date, tweet_id.decode(), tweet_text[tweet_id].decode()))
     sorted_tweets_most_recent.sort(reverse=True)
 
-    val = ""
-    if len(val) >= 1 and len(val[0]) >= 2: 
-        val = sorted_tweets_most_recent[0][1]
+    selected_tweet_id = None
+    selected_tweet_score = -1
+    time_now = int(time.time()) + 1
+    for tweet_date, tweet_id, _ in sorted_tweets_most_recent[:100]:
+        metrics = redis_cli.hgetall(tweet_id)
+        latest_point = max(metrics.keys(), key=int)
+        likes = json.loads(metrics[latest_point])[3]
+        score = likes / (time_now - tweet_date)
+        if score > selected_tweet_score:
+            selected_tweet_id = tweet_id
+            selected_tweet_score = score
 
     return html.Div([
         dcc.Graph(id='graph',
             style={'height': '90vh'}),
-
         dcc.Dropdown(
             id='tweet-selector',
             options=[
-                {'label': tweet[2] if len(tweet[2]) < DISPLAY_CHAR_LIMIT else tweet[2][:DISPLAY_CHAR_LIMIT]+'...', 'value': tweet[1]} for tweet in sorted_tweets_most_recent[:100]
+                {'label': tweet[2] if len(tweet[2]) < DISPLAY_CHAR_LIMIT else tweet[2][:DISPLAY_CHAR_LIMIT]+'...', 'value': tweet[1]} for tweet in sorted_tweets_most_recent[:500]
             ],
-            value=val
+            value=selected_tweet_id  # sorted_tweets_most_recent[0][1]
         )
     ])
 
@@ -56,34 +69,81 @@ def update_figure(tweet_id):
     metrics_raw = redis_cli.hgetall(tweet_id) # {'123456789' : '[13, 0, 4, 2, 5, -1, -1]'}
     followers_raw = redis_cli.hgetall('followers')
     default_follower_count = min(map(int, followers_raw.values()))
+    first_timestamp = None
+    last_timestamp = None
+    metrics_init = None
+    metrics_align = None
+    metrics_cutoff = None
     for timestamp in sorted(metrics_raw.keys(), key=int):
-        for field, value in zip(FIELDS_ORDER, json.loads(metrics_raw[timestamp])):
+        metrics_parsed = json.loads(metrics_raw[timestamp])
+        for field, value in zip(FIELDS_ORDER, metrics_parsed):
             metrics[field].append(value)
         metrics['timestamp'].append(datetime.fromtimestamp(int(timestamp), TIMEZONE).isoformat())
         metrics['followers'].append(int(followers_raw.get(timestamp, default_follower_count)))
+        
+        metrics_vec = dict(zip(FIELDS_ORDER, metrics_parsed))
+        metrics_vec.update({'followers': metrics['followers'][-1]})
+
+        if first_timestamp is None:
+            first_timestamp = int(timestamp)
+            metrics_init = metrics_vec
+        if int(timestamp) - first_timestamp < ALIGN_TIME:
+            metrics_align = metrics_vec
+        if int(timestamp) - first_timestamp < CUTOFF_TIME:
+            metrics_cutoff = metrics_vec
+
+        last_timestamp = int(timestamp)
+    #df = pd.DataFrame(metrics)
+    #fig = px.line(df, x='timestamp', y=['like_count', 'retweet_count']) #, title=tweet_id) 
     
+    metrics_scale = {}
+    for metric_name in FIELDS_ORDER + ['followers']:
+        if metrics_align[metric_name] > metrics_init[metric_name]:
+            metrics_scale[metric_name] = (metrics_align[metric_name] - metrics_init[metric_name]) / (metrics_cutoff[metric_name] - metrics_init[metric_name])
+    scale_factor = min(metrics_scale.values())
+    for metric_name in metrics_scale:
+        metrics_scale[metric_name] /= scale_factor
+
+
     fig = go.Figure()
     
     col_idx = 0
     for col_name in metrics.keys():
         if col_name == 'timestamp' or len(set(metrics[col_name])) < 2:
             continue
-        fig.add_trace(go.Scatter(
-            x=metrics['timestamp'],
-            y=metrics[col_name],
-            name=col_name,
-            yaxis=f'y{col_idx+1}'
-        ))
+
+        fig.add_trace(
+            go.Scatter(
+                x=metrics['timestamp'],
+                y=metrics[col_name],
+                name=col_name,
+                yaxis=f'y{col_idx+1}')
+        )
+
+        startval = metrics_init[col_name]
+        endval = metrics_cutoff[col_name]
+        range_width = metrics_scale.get(col_name, 1) * (endval - startval)
+        yaxis_range = [
+            startval - 0.05 * range_width,
+            startval + 1.05 * range_width
+        ]
         if col_idx:
-            fig.layout[f'yaxis{col_idx+1}'] = {'overlaying':'y','visible':False}
+            fig.layout[f'yaxis{col_idx+1}'] = {'overlaying':'y','visible':False, 'range':yaxis_range}
+        else:
+            fig.layout['yaxis'] = {'range':yaxis_range, 'visible':False}
         
         col_idx += 1
-    
-    fig.layout[f'yaxis'] = {'visible':False}
+    xaxis_range = [
+            datetime.fromtimestamp(int(first_timestamp), TIMEZONE).isoformat(),
+            datetime.fromtimestamp(min(int(last_timestamp), int(first_timestamp)+CUTOFF_TIME), TIMEZONE).isoformat()
+    ]
+    fig.layout['xaxis'] = {'range':xaxis_range} 
+
+    #fig.update_layout(transition_duration=500)
 
     return fig
 
 
 if __name__ == '__main__':
-
-    app.run_server(debug=True, host=os.environ['VIZ_SERVER_HOST'], port = int(os.environ['VIZ_SERVER_PORT']))
+    ssl_context = ('/etc/letsencrypt/live/mathis.h25.io/cert.pem','/etc/letsencrypt/live/mathis.h25.io/privkey.pem')
+    app.run_server(debug=True, host='0.0.0.0', ssl_context=ssl_context)
